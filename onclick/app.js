f6cf0dabdb68deb61ae5263902dace3eafc0dca6
Hanadi Baloch <167871824+Hbaloch19@users.noreply.github.com>
2024-12-16 21:58:25 +0500

    Add files via upload

---
+++ onclick/app.js
@@ -0,0 +1,53 @@
+const Extracontent= document.getElementById("Extracontent");
+const readMoreLink = document.getElementById("readMoreLink");
+readMoreLink.onclick = ()=>{
+    if(Extracontent.style.display==="none")
+ {
+    Extracontent.style.display="inline";
+    readMoreLink.textContent="ReadLess"
+    }
+    else{
+        Extracontent.style.display="none"
+     readMoreLink.textContent="ReadMore"
+    }
+}
+const Extracontent1= document.getElementById("Extracontent1");
+const readMoreLink1 = document.getElementById("readMoreLink1");
+readMoreLink1.onclick = ()=>{
+    if(Extracontent1.style.display==="none")
+ {
+    Extracontent1.style.display="inline";
+    readMoreLink1.textContent="ReadLess"
+    }
+    else{
+     Extracontent1.style.display="none"
+     readMoreLink1.textContent="ReadMore"
+   }
+}
+const Extracontent2= document.getElementById("Extracontent2");
+const readMoreLink2 = document.getElementById("readMoreLink2");
+readMoreLink2.onclick = ()=>{
+    if(Extracontent2.style.display==="none")
+ {
+    Extracontent2.style.display="inline";
+    readMoreLink2.textContent="ReadLess"
+    }
+    else{
+     Extracontent2.style.display="none"
+     readMoreLink2.textContent="ReadMore"
+   }
+}
+
+const Extracontent3= document.getElementById("Extracontent3");
+const readMoreLink3 = document.getElementById("readMoreLink3");
+readMoreLink3.onclick = ()=>{
+    if(Extracontent3.style.display==="none")
+ {
+    Extracontent3.style.display="inline";
+    readMoreLink3.textContent="ReadLess"
+    }
+    else{
+     Extracontent3.style.display="none"
+     readMoreLink3.textContent="ReadMore"
+   }
+}
